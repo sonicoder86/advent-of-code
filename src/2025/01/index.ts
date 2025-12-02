4955e28dadaff4b5cc775b94cff9ed3404df1cfa
Gabor Soos <gabor.soos@byborgenterprises.com>
2025-12-02 17:01:54 +0100

    part 1

--- src/2025/01/index.ts
+++ src/2025/01/index.ts
@@ -1,6 +1,6 @@
 import { readLines } from "../../utils.js";
 
-const lines = readLines(import.meta.url, "sample.txt");
+const lines = readLines(import.meta.url, "input.txt");
 
 let position = 50;
 let zeroCount = 0;
@@ -10,10 +10,11 @@ for (const line of lines) {
   const distance = parseInt(line.slice(1), 10);
 
   if (direction === "L") {
-    position = (position - distance % 100 + 100) % 100;
+    position = position - distance % 100 + 100;
   } else {
-    position = (position + distance) % 100;
+    position = (position + distance);
   }
+  position = position % 100;
 
   if (position === 0) {
     zeroCount++;
