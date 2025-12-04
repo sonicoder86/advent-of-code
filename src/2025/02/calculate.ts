f4ec2ff3819958b89fec33be5b78560354fc9210
Gabor Soos <gabor.soos@byborgenterprises.com>
2025-12-04 14:34:52 +0100

    day 2 part 2

--- src/2025/02/calculate.ts
+++ src/2025/02/calculate.ts
@@ -25,5 +25,27 @@ export function calculatePart1(lines: string[]): number {
 }
 
 export function calculatePart2(lines: string[]): number {
-  return 0;
+  const ranges = lines[0].split(',');
+  const invalidIds: number[] = [];
+
+  function isInvalid(id: number): boolean {
+    const str = id.toString();
+    return /^(.+)\1+$/.test(str);
+  }
+
+  for (let i = 0; i < ranges.length; i++) {
+    const [rangeStart, rangeEnd] = ranges[i].split('-');
+
+    for (
+      let j = Number.parseInt(rangeStart, 10);
+      j <= Number.parseInt(rangeEnd, 10);
+      j++
+    ) {
+      if (isInvalid(j)) {
+        invalidIds.push(j);
+      }
+    }
+  }
+
+  return invalidIds.reduce((acc, num) => acc + num, 0);
 }
--- src/2025/02/index.ts
+++ src/2025/02/index.ts
@@ -1,8 +1,10 @@
 import { readLines } from '../../utils.js';
-import { calculatePart1 } from './calculate.js';
+import { calculatePart1, calculatePart2 } from './calculate.js';
 
 const lines = readLines(import.meta.url, 'input.txt');
 
 const resultPart1 = calculatePart1(lines);
+const resultPart2 = calculatePart2(lines);
 
 console.log(`Part 1: ${resultPart1}`);
+console.log(`Part 2: ${resultPart2}`);
